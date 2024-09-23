d49366da1f8fe00cdc3c44251ab5b322da3438c0
Yash <yashvaghela432@gmail.com>
2024-09-23 19:09:18 +0100

    cipher added

---
+++ decode_cipher.py
@@ -0,0 +1,21 @@
+def decode_vigenere_cipher(message, keyword):
+    
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    final_messsage = ""
+    key = ""
+    length = 0
+    
+    for letter in message:
+        if letter in alphabet:
+            key += keyword[length % len(keyword)]
+            length += 1
+        else:
+            key += letter
+
+    for i in range(len(message)):
+        if message[i] in alphabet:
+            final_messsage += alphabet[(alphabet.index(message[i]) + alphabet.index(key[i])) % 26]
+        else:
+            final_messsage += message[i]
+    
+    return final_messsage
